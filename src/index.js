264274fbf3c88b10f84e54ee1fe5b9157b205868
andrewjkelly <andrewjkellyservices@gmail.com>
2017-09-23 21:08:18 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -43,6 +43,19 @@ app.post('/listen', function (req, res) {
     
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 // PRIMARY ELSE IF messasge
 
     var m1 = "Welcome, to NameBrothers: \"Best 2018 Webhosting\"";
@@ -54,38 +67,38 @@ app.post('/listen', function (req, res) {
     var m2 = "this is a helpful message";
     var a2 ="HELP";
 
-    var m3= "Damint";
-    var a3="";
+        var m3= "";
+        var a3="";
 
-    var m4="";
-    var a4="";
+            var m4="";
+            var a4="";
 
-    var m5="";
-    var a5="";
+            var m5="";
+            var a5="";
 
-    var m6="";
-    var a6="";
+            var m6="";
+            var a6="";
 
-    var m7="";
-    var a7="";
-            
-    var m8="";
-    var a8="";
+            var m7="";
+						var a7="";
+						
+						var m8="";
+						var a8="";
 
-    var m9="";
-    var a9="";
+						var m9="";
+						var a9="";
 
-    var m10="";
-    var a10="";
+						var m10="";
+						var a10="";
 
-    var m11="";
-    var a11="";
+						var m11="";
+						var a11="";
 
-    var m12="";
-    var a12="";
+						var m12="";
+						var a12="";
 
-    var m13="";
-    var a13="";
+						var m13="";
+						var a13="";
 
 
             
@@ -96,8 +109,8 @@ app.post('/listen', function (req, res) {
     var m31= "";
     var a31="";
 
-    var m32= "";
-    var a32="";
+        var m32= "";
+        var a32="";
 			
 
 //--------------------------
@@ -106,8 +119,9 @@ app.post('/listen', function (req, res) {
     var m61="";
     var a61="";
 
-    var m62="";
-    var a62="";
+
+        var m62="";
+        var a62="";
 		
 
 //--------------------------
@@ -116,211 +130,213 @@ app.post('/listen', function (req, res) {
 
 
 
-    // response
+	// response
 
-    var payload= req.body['payload']; if(payload = (a1)){
+    var payload= req.body['payload'];
+    
+    if(payload ==String(a1)){
+        message=(m1);
+    } else if (payload ==String(a2)){
         message=(m2);
-    } else if (payload = (a2)){
-        message=(m2);
-    } else if(payload = (a3)){
+    } else if(payload ==String(a3)){
 		message=(m3);
-	} else if(payload =(a4)){
+		} else if(payload ==String(a4)){
 		message=(m4);
-	} else if(payload =(a5)){
+		} else if(payload ==String(a5)){
 		message=(m5);
-	} else if(payload =(a6)){
+		} else if(payload ==String(a6)){
 		message=(m6);
-	} else if(payload =(a7)){
+		} else if(payload ==String(a7)){
 		message=(m7);
-    } else if(payload =(a8)){
+    } else if(payload ==String(a8)){
 		message=(m8);
-    } else if(payload =(a9)){
+    } else if(payload ==String(a9)){
 		message=(m9);
-    } else if(payload =(a10)){
+    } else if(payload ==String(a10)){
 		message=(m10);
-    } else if(payload =(a11)){
+    } else if(payload ==String(a11)){
 		message=(m11);
-    } else if(payload =(a12)){
+    } else if(payload ==String(a12)){
 		message=(m12);
-    } else if(payload =(a13)){
+    } else if(payload ==String(a13)){
 		message=(m13);
-    } else if(payload =(a14)){
+    } else if(payload ==String(a14)){
 		message=(m14);
-	} else if(payload =(a15)){
+		} else if(payload ==String(a15)){
 		message=(m15);
-	} else if(payload =(a16)){
+		} else if(payload ==String(a16)){
 		message=(m16);
-	} else if(payload =(a17)){
+		} else if(payload ==String(a17)){
 		message=(m17);
-    } else if(payload =(a18)){
+    } else if(payload ==String(a18)){
 		message=(m18);
-    } else if(payload =(a19)){
+    } else if(payload ==String(a19)){
 		message=(m19);
-    } else if(payload =(a20)){
+    } else if(payload ==String(a20)){
 		message=(m20);
-    } else if(payload =(a21)){
+    } else if(payload ==String(a21)){
 		message=(m21);
-	} else if(payload =(a22)){
+		} else if(payload ==String(a22)){
 		message=(m22);
-	} else if(payload =(a23)){
+		} else if(payload ==String(a23)){
 		message=(m23);
-    } else if(payload =(a24)){
+    } else if(payload ==String(a24)){
 		message=(m24);
-    } else if(payload =(a25)){
+    } else if(payload ==String(a25)){
 		message=(m25);
-    } else if(payload =(a26)){
+    } else if(payload ==String(a26)){
 		message=(m26);
-    } else if(payload =(a27)){
+    } else if(payload ==String(a27)){
 		message=(m27);
-    } else if(payload =(a28)){
+    } else if(payload ==String(a28)){
 		message=(m28);
-    } else if(payload =(a29)){
+    } else if(payload ==String(a29)){
 		message=(m29);
-    } else if(payload =(a30)){
+    } else if(payload ==String(a30)){
 		message=(m30);
-    } else if(payload =(a31)){
+    } else if(payload ==String(a31)){
 		message=(m31);
-	} else if(payload =(a32)){
+		} else if(payload ==String(a32)){
 		message=(m32);
-	} else if(payload =(a33)){
+		}else if(payload ==String(a33)){
 		message=(m33);
-	} else if(payload =(a34)){
+		}else if(payload ==String(a34)){
 		message=(m34);
-	} else if(payload =(a35)){
+		}else if(payload ==String(a35)){
 		message=(m35);
-	} else if(payload =(a36)){
+		}else if(payload ==String(a36)){
 		message=(m36);
-	} else if(payload =(a37)){
-		message=(m37);
-    } else if(payload =(a38)){
-		message=(m38);
-    } else if(payload =(a39)){
-		message=(m39);
-    } else if(payload =(a40)){
-		message=(m40);
-    } else if(payload =(a41)){
-		message=(m41);
-	} else if(payload =(a42)){
-		message=(m42);
-	} else if(payload =(a43)){
+		} else if(payload ==String(a37)){
+			message=(m37);
+    } else if(payload ==String(a38)){
+			message=(m38);
+    } else if(payload ==String(a39)){
+			message=(m39);
+    } else if(payload ==String(a40)){
+			message=(m40);
+    } else if(payload ==String(a41)){
+			message=(m41);
+		} else if(payload ==String(a42)){
+			message=(m42);
+		} else if(payload ==String(a43)){
 		message=(m43);
-    } else if(payload =(a44)){
+    } else if(payload ==String(a44)){
 		message=(m44);
-    } else if(payload =(a45)){
+    } else if(payload ==String(a45)){
 		message=(m45);
-    } else if(payload =(a46)){
+    } else if(payload ==String(a46)){
 		message=(m46);
-    } else if(payload =(a47)){
+    } else if(payload ==String(a47)){
 		message=(m47);
-    } else if(payload =(a48)){
+    } else if(payload ==String(a48)){
 		message=(m48);
-    } else if(payload =(a49)){
+    } else if(payload ==String(a49)){
 		message=(m49);
-    } else if(payload =(a50)){
+    } else if(payload ==String(a50)){
 		message=(m50);
-    } else if(payload =(a51)){
+    } else if(payload ==String(a51)){
 		message=(m51);
-    } else if(payload =(a52)){
+    } else if(payload ==String(a52)){
 		message=(m52);
-    } else if(payload =(a53)){
+    } else if(payload ==String(a53)){
 		message=(m53);
-    } else if(payload =(a54)){
+    } else if(payload ==String(a54)){
 		message=(m54);
-    } else if(payload =(a55)){
+    } else if(payload ==String(a55)){
 		message=(m55);
-    } else if(payload =(a56)){
+    } else if(payload ==String(a56)){
 		message=(m56);
-    } else if(payload =(a57)){
+    } else if(payload ==String(a57)){
 		message=(m57);
-    } else if(payload =(a58)){
+    } else if(payload ==String(a58)){
 		message=(m58);
-    } else if(payload =(a59)){
+    } else if(payload ==String(a59)){
 		message=(m59);
-    } else if(payload =(a60)){
+    } else if(payload ==String(a60)){
 		message=(m60);
-    } else if(payload =(a61)){
+    } else if(payload ==String(a61)){
 		message=(m61);
-	} else if(payload =(a62)){
+		} else if(payload ==String(a62)){
 		message=(m62);
-	} else if(payload =(a63)){
+		} else if(payload ==String(a63)){
 		message=(m63);
-	} else if(payload =(a64)){
+		} else if(payload ==String(a64)){
 		message=(m64);
-	} else if(payload =(a65)){
+		} else if(payload ==String(a65)){
 		message=(m65);
-    } else if(payload =(a66)){
+    } else if(payload ==String(a66)){
 		message=(m66);
-    } else if(payload =(a67)){
+    } else if(payload ==String(a67)){
 		message=(m67);
-    } else if(payload =(a68)){
+    } else if(payload ==String(a68)){
 		message=(m68);
-    } else if(payload =(a69)){
+    } else if(payload ==String(a69)){
 		message=(m69);
-    } else if(payload =(a70)){
+    } else if(payload ==String(a70)){
 		message=(m70);
-    } else if(payload =(a71)){
+    } else if(payload ==String(a71)){
 		message=(m71);
-    } else if(payload =(a72)){
+    } else if(payload ==String(a72)){
 		message=(m72);
-    } else if(payload =(a73)){
+    } else if(payload ==String(a73)){
 		message=(m73);
-    } else if(payload =(a74)){
+    } else if(payload ==String(a74)){
 		message=(m74);
-    } else if(payload =(a75)){
+    } else if(payload ==String(a75)){
 		message=(m75);
-    } else if(payload =(a76)){
+    } else if(payload ==String(a76)){
 		message=(m76);
-    } else if(payload =(a77)){
+    } else if(payload ==String(a77)){
 		message=(m77);
-    } else if(payload =(a78)){
+    } else if(payload ==String(a78)){
 		message=(m78);
-    } else if(payload =(a79)){
+    } else if(payload ==String(a79)){
 		message=(m79);
-    } else if(payload =(a80)){
+    } else if(payload ==String(a80)){
 		message=(m80);
-    } else if(payload =(a81)){
+    } else if(payload ==String(a81)){
 		message=(m81);
-    } else if(payload =(a82)){
+    }else if(payload ==String(a82)){
 		message=(m82);
-    } else if(payload =(a83)){
+    }else if(payload ==String(a83)){
 		message=(m83);
-    } else if(payload =(a84)){
+    }else if(payload ==String(a84)){
 		message=(m84);
-    } else if(payload =(a85)){
+    }else if(payload ==String(a85)){
 		message=(m85);
-    } else if(payload =(a86)){
+    } else if(payload ==String(a86)){
 		message=(m86);
-    } else if(payload =(a87)){
+    } else if(payload ==String(a87)){
 		message=(m87);
-    } else if(payload =(a88)){
+    } else if(payload ==String(a88)){
 		message=(m88);
-    } else if(payload =(a89)){
+    } else if(payload ==String(a89)){
 		message=(m89);
-    } else if(payload =(a90)){
+    } else if(payload ==String(a90)){
 		message=(m90);
-    } else if(payload = (a91)){
+    } else if(payload == String(a91)){
 		message=(m91);
-	} else if(payload =(a92)){
+		} else if(payload ==String(a92)){
 		message=(m92);
-	} else if(payload =(a93)){
+		} else if(payload ==String(a93)){
 		message=(m93);
-	} else if(payload =(a94)){
+		} else if(payload ==String(a94)){
 		message=(m94);
-	} else if(payload =(a95)){
+		} else if(payload ==String(a95)){
 		message=(m95);
-	} else if(payload =(a96)){
+		} else if(payload ==String(a96)){
 		message=(m96);
-    } else if(payload =(a97)){
+    } else if(payload ==String(a97)){
 		message=(m97);
-    } else if(payload =(a98)){
+    } else if(payload ==String(a98)){
 		message=(m98);
-    } else if(payload =(a99)){
+    }else if(payload ==String(a99)){
 		message=(m99);
-    } else if(payload =(a100)){
+    }else if(payload ==String(a100)){
 		message=(m100);
-    } else{
-		message=(m3);
-	}
+    } else {
+		message=(m1);
+		}
     
 
 	//done
